9fc5c96347b3b65b145f1a24fceed0b26e81b026
zachkwak <zachkwak@gmail.com>
2026-02-21 17:19:46 -0800

    Update w/ Personal Desc

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -12,5 +12,7 @@ def main():
 
     print(f"{name}, Welcome to the CSS course!")
 
+    print("Hi! I'm Zach from California. My hobbies are baking, gardening, and sewing! I enjoy working with my hands and being crafty, as you can tell by my hobbies probably. I'm excited to work with all of you!")
+
 if __name__ == "__main__":
     main()
